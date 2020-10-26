798a024cde0b05841660b15c302d005baf1f56d6
Ian <ianelicha@gmail.com>
2020-10-25 19:07:17 -0600

    ✔Profile settings working

--- components/Settings/Profile.tsx
+++ components/Settings/Profile.tsx
@@ -1,13 +1,29 @@
 import React, { useContext } from "react";
-import { ScrollView } from "react-native";
+import { ScrollView, Alert } from "react-native";
+import {
+  useAcceptFriendRequestMutation,
+  useDeleteFriendRequestMutation,
+  useLeaveServerMutation,
+  useRemoveFriendMutation,
+} from "../../generated/graphql";
 import { AvatarNameCombo } from "../AvatarNameCombo";
 import { Loading } from "../Loading";
 import { userContext } from "../Providers/UserProvider";
 import { RectangleScroller } from "../RectangleScroller";
 import { Widget } from "../Widget";
 
+/**
+ * async (id) => {
+            await leaveServer({variables: {id}})
+            await refetch()
+          }
+ */
 export function Profile() {
-  const { user, loading } = useContext(userContext);
+  const { user, loading, refetchUser } = useContext(userContext);
+  const [leaveServer] = useLeaveServerMutation();
+  const [unfriend] = useRemoveFriendMutation();
+  const [removeFriendRequest] = useDeleteFriendRequestMutation();
+  const [acceptFriendRequest] = useAcceptFriendRequestMutation();
   return loading ? (
     <Loading />
   ) : (
@@ -24,7 +40,77 @@ export function Profile() {
             key: id,
             name,
           }))}
-          onNegative={() => null}
+          placeholder="You haven't joined any servers yet."
+          onNegative={(_, server) => {
+            Alert.alert(
+              "Leave server",
+              `Are you sure you want to leave "${server?.name ?? "???"}"?`,
+              [
+                { text: "Cancel", style: "cancel" },
+                {
+                  text: "Leave",
+                  style: "destructive",
+                  onPress: async () => {
+                    await leaveServer({ variables: { id: server.key } });
+                    await refetchUser();
+                  },
+                },
+              ],
+              { cancelable: true }
+            );
+          }}
+        />
+      </Widget>
+      <Widget
+        title="Your friends"
+        action={{
+          icon: "plus",
+          onPress: () => Alert.alert("Uh oh", "TBI"), //TODO: implement this lmao
+        }}
+      >
+        <RectangleScroller
+          showIcon
+          elements={user!.friends.map(({ name, id }) => ({
+            key: id,
+            name,
+          }))}
+          placeholder="You don't have any friends yet."
+          onNegative={(_, friend) => {
+            Alert.alert(
+              "Leave server",
+              `Are you sure you want to unfriend "${friend.name ?? "???"}"?`,
+              [
+                { text: "Cancel", style: "cancel" },
+                {
+                  text: "Unfriend",
+                  style: "destructive",
+                  onPress: async () => {
+                    await unfriend({ variables: { id: friend.key } });
+                    await refetchUser();
+                  },
+                },
+              ],
+              { cancelable: true }
+            );
+          }}
+        />
+      </Widget>
+      <Widget title="Your friend requests">
+        <RectangleScroller
+          showIcon
+          elements={user!.friendRequests.map(({ name, id }) => ({
+            key: id,
+            name,
+          }))}
+          placeholder="No friends requests here, yet."
+          onNegative={async (id) => {
+            await removeFriendRequest({ variables: { id } });
+            await refetchUser();
+          }}
+          onPositive={async (id) => {
+            await acceptFriendRequest({ variables: { id } });
+            await refetchUser();
+          }}
         />
       </Widget>
     </ScrollView>
